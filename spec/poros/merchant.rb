3f0fa3507fab1859fbe8e89cd55e58fde26b7407
Thomas Casady <tscasady@gmail.com>
2023-02-15 17:40:26 -0600

    Add merchant poro and test.

---
+++ spec/poros/merchant.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Merchant do
+  let!(:data) { { name: 'Test Merchant' } }
+  let!(:merchant) { Merchant.new(data) }
+
+  it 'has readable attributes' do
+    expect(merchant.name).to eq 'Test Merchant'
+  end
+end
